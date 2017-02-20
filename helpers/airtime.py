bc20759b28944602be1022796a40b7584f2d6a7a
mmellott <matthew.mellott@yahoo.com>
2017-02-19 19:01:51 -0700

    add AirtimeObserver class that fixes dBm bug

--- helpers/airtime.py
+++ helpers/airtime.py
@@ -3,38 +3,53 @@
 from __future__ import division
 
 import subprocess
-import re
 import sys
 import time
 
-def parse_survey_time(line):
-    return float(re.findall(r'\d+ ms', line)[0].split()[0]) / 1000
+class AirtimeObserver(object):
 
-def iw_survey_dump(dev='wlan0'):
-    cmd = ['iw', dev, 'survey', 'dump']
-    output = subprocess.check_output(cmd).split('\n')
+    def __init__(self, dev='wlan0'):
+        self.cmd = ['iw', dev, 'survey', 'dump']
+        self.iw_survey_dump()
 
-    for i in xrange(len(output)):
-        line = output[i]
-        if re.search(r'frequency:.*\[in use\]$', line):
-            active = parse_survey_time(output[i + 1])
-            busy = parse_survey_time(output[i + 2])
-            recv = parse_survey_time(output[i + 3])
-            xmit = parse_survey_time(output[i + 4])
+    def skip(self, i, to):
+        while i < len(self.output):
+            if self.output[i] == to:
+                break
+            else:
+                i += 1
 
-            return active, busy, recv, xmit
+        return i
+
+    def iw_survey_dump(self):
+        self.output = subprocess.check_output(self.cmd).split()
+
+        i = 0
+        i = self.skip(i, '[in')
+        i = self.skip(i, 'active')
+
+        self.active = float(self.output[i + 2])
+
+        i = self.skip(i, 'transmit')
+
+        self.transmit = float(self.output[i + 2])
+
+    def airtime(self):
+        old_active = self.active
+        old_transmit = self.transmit
+
+        self.iw_survey_dump()
+
+        return (self.transmit - old_transmit) / (self.active - old_active)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         sleep_time = int(sys.argv[1])
     else:
-        sleep_time = 10
+        sleep_time = 1
 
-    active_start, _, _, xmit_start = iw_survey_dump()
+    ao = AirtimeObserver()
     while True:
         time.sleep(sleep_time)
-        active_end, _, _, xmit_end = iw_survey_dump()
-
-        print (xmit_end - xmit_start) / (active_end - active_start)
-        active_start, xmit_start = active_end, xmit_end
+        print ao.airtime()
 
