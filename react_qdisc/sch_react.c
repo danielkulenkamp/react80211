9f6c53be9532e35146e9276c5ff95bf0ec5cc6bb
mmellott <matthew.mellott@yahoo.com>
2017-03-23 17:46:32 -0700

    add react_qdisc

---
+++ react_qdisc/sch_react.c
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/skbuff.h>
+#include <net/pkt_sched.h>
+
+static int react_enqueue(struct sk_buff *skb, struct Qdisc *sch)
+{
+	/* Got more up-to-date data, empty queue */
+	while (__skb_dequeue(&sch->q));
+
+	return qdisc_enqueue_tail(skb, sch);
+}
+
+static int react_init(struct Qdisc *sch, struct nlattr *opt)
+{
+	/* Only keep the most up-to-date control packet */
+	sch->limit = 1;
+
+	//sch->flags &= ~TCQ_F_CAN_BYPASS;
+
+	return 0;
+}
+
+struct Qdisc_ops react_qdisc_ops __read_mostly = {
+	.id		=	"react",
+	.priv_size	=	0,
+	.enqueue	=	react_enqueue,
+	.dequeue	=	qdisc_dequeue_head,
+	.peek		=	qdisc_peek_head,
+	.drop		=	qdisc_queue_drop,
+	.init		=	react_init,
+	.reset		=	qdisc_reset_queue,
+	.change		=	react_init,
+	//.dump		=	fifo_dump,
+	.owner		=	THIS_MODULE,
+};
+EXPORT_SYMBOL(react_qdisc_ops);
