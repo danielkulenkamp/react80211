266c56850f3cd6f1fcb07bde3096dff4729ad6df
mmellott <matthew.mellott@yahoo.com>
2018-03-03 11:12:08 -0700

    free_nodes: convert time for me pls

--- utils/free_nodes.py
+++ utils/free_nodes.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import pendulum
 
 import sys
 
@@ -11,7 +12,9 @@ d.get('http://boss.wilab2.ilabt.iminds.be/reservation/index.php/reserve/new')
 raw_input('Hit Enter after you have logged in...')
 
 while True:
-    print d.find_element_by_name('startdate').get_attribute('value')
+    today = d.find_element_by_name('startdate').get_attribute('value')
+    print today
+    year, month, day = map(int, today.split('-'))
 
     for hour in xrange(24):
         time_fmt = Keys.BACKSPACE*5 + '{:02}:{:02}'
@@ -23,13 +26,12 @@ while True:
         d.execute_script('showhide("free_ZOTAC")')
 
         # print free nodes
-        sys.stdout.write('{:02}:'.format(hour))
-        nodes = []
-        for node in d.find_elements_by_partial_link_text('zotac'):
-            nodes.append(node.text)
-        nodes.sort()
-        sys.stdout.write(' ' + ' '.join(nodes))
-        sys.stdout.write('\n')
+        time = pendulum.create(year, month, day, hour, tz='Europe/Brussels')
+        time_phx = time.in_timezone('America/Phoenix')
+        nodes = d.find_elements_by_partial_link_text('zotac')
+
+        sys.stdout.write("[{:02}] {}: {}\n".format(hour,
+                time_phx.format("%a %I %p"), len(nodes)))
 
     raw_input('Hit Enter to re-run...')
     sys.stdout.write('\n')
