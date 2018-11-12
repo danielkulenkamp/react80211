183be40c4ab9ecaf95e56d0526cca138c7234d7f
mmellott <matthew.mellott@yahoo.com>
2018-11-11 17:40:32 -0700

    data: badass path finding

--- utils/data.py
+++ utils/data.py
@@ -212,16 +212,19 @@ def plot_thr(trial_dir, proto='tcp', plot=True):
     plt.show()
 
 def get_graphs(node_dir):
-
     # Create a dictionary that maps IP addresses to testbed node names
     ip_to_node = {}
-    for path in glob('{}/zotac*/192.168.0.1'.format(node_dir)):
+    for path in glob('{}/*/192.168.0.1'.format(node_dir)):
         node = path.split('/')[-2]
 
+        found = False
         for line in open(path):
             if re.search('PING', line):
+                found = True
                 ip_to_node[line.split()[4]] = node
                 break
+        assert found, "Can't find IP address for node {}".format(node)
+    assert len(ip_to_node) != 0, "Didn't find any nodes"
 
     # Graph G100 has edges between all 100% transmission probability links
     G100 = nx.DiGraph()
@@ -229,36 +232,41 @@ def get_graphs(node_dir):
     Gall = nx.DiGraph()
 
     # Add edges to graphs
-    for path in glob('{}/zotac*/192.*'.format(node_dir)):
+    for path in glob('{}/*/192.*'.format(node_dir)):
         to_node = from_node = packet_loss = None
 
         for line in open(path):
             parts = line.split()
 
             if re.search('PING', line):
-                assert(to_node is None and from_node is None)
+                assert to_node is None and from_node is None, \
+                        'More than one line with "PING" in it?'
                 to_node = ip_to_node[parts[1]]
                 from_node = ip_to_node[parts[4]]
 
             if re.search('packet loss', line):
                 for i in xrange(len(parts)):
                     if parts[i] == 'packet':
-                        assert(packet_loss is None)
-                        assert(parts[i + 1] == 'loss,')
+                        assert packet_loss is None, \
+                                'More than one line with "packet loss" in it?'
+                        assert parts[i + 1] == 'loss,', \
+                                'Packet loss not where expected in line'
                         packet_loss = float(parts[i - 1].strip('%'))/100.0
 
-        assert(to_node is not None and from_node is not None and
-                packet_loss is not None)
-        assert(not(packet_loss < 0.0) and not(packet_loss > 1.0))
+        assert to_node is not None and from_node is not None, \
+                "Didn't find both to and from nodes"
+        assert packet_loss is not None, "Didn't find packet loss"
+        assert not(packet_loss < 0.0) and not(packet_loss > 1.0), \
+                'Packet loss out of range: {}'.format(packet_loss)
         transmission_prob = 1 - packet_loss
         if transmission_prob == 1.0:
             G100.add_edge(from_node, to_node)
         if transmission_prob > 0.0:
             Gall.add_edge(from_node, to_node)
 
-    # Sanity check, all edges in G100 should be in Gall
     for n1, n2 in G100.edges():
-        assert(Gall.has_edge(n1, n2))
+        assert Gall.has_edge(n1, n2), \
+                'Sanity check failed, all edges in G100 should be in Gall'
 
     return G100, Gall
 
@@ -269,44 +277,34 @@ def net_graph(node_dir):
         nx.draw_networkx(g)
         plt.show()
 
-def find_paths(node_dir, length=1):
+def find_paths(node_dir, length=4):
     length = int(length)
 
     G100, Gall = get_graphs(node_dir)
-    paths = []
 
-    # For each pair of nodes, check if the length of the shortest paths between
-    # these nodes in G100 is if of the target length. If it is, check that there
-    # is not a shorter path between the nodes in Gall. If there is not a shorter
-    # path then the shortest paths in G100 are "high-quality" paths. This is
-    # because each link in the path has a 100% transmission probability and
-    # there are no lower probability links that would form a shorter path
-    # between the pair of nodes.
-    for n1 in G100:
-        for n2 in G100:
-            try:
-                if nx.shortest_path_length(G100, n1, n2) == \
-                        nx.shortest_path_length(Gall, n1, n2) and \
-                        nx.shortest_path_length(G100, n1, n2) == length:
-                    for path in nx.all_shortest_paths(G100, n1, n2):
-                        paths.append(path)
-            except nx.NetworkXNoPath:
-                pass
+    def valid(p):
+        try:
+            for G in (G100, Gall):
+                for n1, n2 in ((p[0], p[-1]), (p[-1], p[0])):
+                    g = G.subgraph(p)
+                    if nx.shortest_path_length(g, n1, n2) != len(p) - 1:
+                        return False
+        except nx.NetworkXNoPath:
+            return False
+        return True
 
-    # Filter out uni-directional paths and print bi-directional paths
-    for i in range(len(paths)):
-        if len(paths[i]) == 0:
-            break
+    def check(p):
+        if len(p) - 1 == length:
+            print p
+            return
 
-        for j in range(len(paths)):
-            if i != j:
-                paths[j].reverse()
-                if paths[i] == paths[j]:
-                    print paths[i]
-                    paths[i] = []
-                    paths[j] = []
-                    break
-                paths[j].reverse()
+        for n in G100:
+            q = p + (n,)
+            if valid(q):
+                check(q)
+
+    for n in G100:
+        check((n,))
 
 def find_star(node_dir, plot=False):
     if isinstance(plot, basestring):
